2bfc18ad213e755d1a4395981116a73c504f6713
Brendan Ahearn <91020994+Arcanaut@users.noreply.github.com>
2022-04-12 16:08:43 -0500

    successfully integrated chakra-ui darkmode

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
+
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    <ChakraProvider>
+     <ColorModeScript initialColorMode="light"></ColorModeScript>
     <App />
+    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
