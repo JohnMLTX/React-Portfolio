d1a910f22e7ed79d12f17e325ab51bde06e30cea
John Lenard <104034804+JohnMLTX@users.noreply.github.com>
2022-04-21 16:31:43 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,40 +1,41 @@
-
 import React from "react";
 
-import  Contact  from "./components/Contact"
+import Contact from "./components/Contact";
 
 import { IconButton } from "@chakra-ui/button";
 import { useColorMode } from "@chakra-ui/color-mode";
 import { Flex, VStack, Heading, Spacer } from "@chakra-ui/layout";
 import { FaSun, FaMoon } from "react-icons/fa";
+import ContactForm from "./components/Contact";
 
 //TODO: Get App to read imported ContactMe component and return it.
 
-// import reactDom from "react-dom";
+//import reactDom from "react-dom";
 
 function App() {
-   
-  const { colorMode, toggleColorMode } = useColorMode(); 
+  const { colorMode, toggleColorMode } = useColorMode();
   const isDark = colorMode === "dark";
   return (
     <div>
       <VStack>
-          <Flex w="100%">
-                <Heading ml="2" size="md" fontWeight='extrabold' 
-             color='blue.500' >Portfolio</Heading>
+        <Flex w="100%">
+          <Heading ml="2" size="md" fontWeight="extrabold" color="blue.500">
+            Portfolio
+          </Heading>
 
-        <Spacer></Spacer>
+          <Spacer></Spacer>
 
-       <IconButton ml={9} icon={isDark ? <FaSun /> : <FaMoon />} 
-       isRound="true" onClick={toggleColorMode}></IconButton>
-      </Flex>
-    </VStack>
-
-      <Contact />
-
-      </div>
-    
+          <IconButton
+            ml={9}
+            icon={isDark ? <FaSun /> : <FaMoon />}
+            isRound="true"
+            onClick={toggleColorMode}
+          ></IconButton>
+        </Flex>
+      </VStack>
+      <ContactForm />
+    </div>
   );
 }
-   
+
 export default App;
